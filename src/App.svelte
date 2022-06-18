f6af9d1f3644053c1b4de34bd338f118c66f1630
elucve <dpalpatr@gmail.com>
2022-06-18 23:42:08 +0300

    ready

---
+++ src/App.svelte
@@ -0,0 +1,11 @@
+<script>
+</script>
+
+<main>
+  <h1 class="bg-black p-4 text-center text-white">
+    This element should have (bg-black p-4 text-center text-white).
+  </h1>
+</main>
+
+<style>
+</style>
