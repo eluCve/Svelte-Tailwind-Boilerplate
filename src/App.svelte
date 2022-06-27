dcd6376bc83518b02486cd21f915a79207cd2d2e
eluCve <dpalpatr@hotmail.com>
2022-06-27 23:26:29 +0300

    minor adjustments

--- src/App.svelte
+++ src/App.svelte
@@ -3,7 +3,8 @@
 
 <main>
   <h1 class="bg-black p-4 text-center text-white">
-    This element should have (bg-black p-4 text-center text-white).
+    This element should have black background with 1rem padding and centered
+    white text. If it does, congrats tailwind works!
   </h1>
 </main>
 
